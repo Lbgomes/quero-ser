765052c03b643068eae1b002d963ece4e43f615e
Murilo Gomes <murilolbgomes@gmail.com>
2021-01-08 19:45:21 -0300

    Quero ser Intelitrader!

---
+++ Caixa_Eletronico/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Caixa_Eletronico {
+        class Program
+    {
+        /*
+                --ROLETA ROMANA--
+                https://dojopuzzles.com/problems/roleta-romana/
+        */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Caixa Eletronico");
+            System.Console.WriteLine();
+            var valor = 0;
+
+
+            Console.Write("Digite o valor do saque: ");
+            if (int.TryParse(Console.ReadLine(), out valor))
+            {
+                int notas100 = valor / 100;
+                int resto = valor - (notas100 * 100);
+
+                int notas50 = resto / 50;
+                resto -= notas50 * 50;
+
+                int notas20 = resto / 20;
+                resto -= notas20 * 20;
+
+                int notas10 = resto / 10;
+                resto -= notas10 * 10;
+
+                int notas5 = resto / 5;
+                resto -= notas5 * 5;
+                
+                int notas1 = resto / 1;
+                resto -= notas1 * 1;
+                
+                Console.WriteLine($"\nQuantidade de notas:\nNotas de 100R$:  {notas100}\n");
+                Console.WriteLine($"Notas de 50R$:  {notas50}\n");
+                Console.WriteLine($"Notas de 20R$:  {notas20}\n");
+                Console.WriteLine($"Notas de 10R$:  {notas10}\n");
+                Console.WriteLine($"Notas de 5R$:  {notas5}\n");
+                Console.WriteLine($"Notas de 1R$:  {notas1}\n");
+            }
+        }
+    }
+}
---
+++ Roleta_Romana/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Roleta_Romana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                --ROLETA ROMANA--
+                https://dojopuzzles.com/problems/roleta-romana/
+            */
+
+
+            int pessoas = 5;
+            int passo = 2;
+            int inicio = 1;
+
+            int count = 1;
+            int proximo = inicio + passo;
+
+
+
+            do
+            {
+                Console.WriteLine(proximo.ToString());
+
+
+                proximo = proximo + passo;
+
+
+                if (proximo > pessoas)
+                {
+                    proximo = proximo - pessoas;
+                }
+
+                count++;
+            } while (count < pessoas);
+
+        }
+    }
+}
